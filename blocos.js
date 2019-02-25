2ebe2611d28c802839f03ddb4c070bae93078cdf
guinhomoraes <guilhermeguilherme98@gmail.com>
2019-02-25 20:36:53 -0300

    ajustes dos blocos

---
+++ blocos.js
@@ -0,0 +1,45 @@
+$(document).ready(function() 
+{
+	
+
+	//basico
+	$('#html').attr('data-code', '<html>');
+	$('#barrahtml').attr('data-code', '</html>');
+	$('#head').attr('data-code', '<head>');
+	$('#barrahead').attr('data-code', '</head>');
+	$('#body').attr('data-code', '<body>');
+	$('#barrabody').attr('data-code', '</body>');
+	$('#link').attr('data-code', '<link rel="stylesheet" type="text/css" href="">');
+	$('#script').attr('data-code', '<script type="text/javascript" src=""></script>');
+	$('#div').attr('data-code', '<div>');
+	$('#barradiv').attr('data-code', '</div>');
+	$('#paragrafo').attr('data-code', '<p>Parágrafo</p>');
+	$('#titulo').attr('data-code', '<h1>Título</h1>');
+	$('#span').attr('data-code', '<span>SPAN</span>');
+
+	//lista
+	$('#ul').attr('data-code', '<ul>');
+	$('#barraul').attr('data-code', '</ul>');
+	$('#li').attr('data-code', '<li>Lista</li>');
+
+
+	//tabela
+	$('#table').attr('data-code', '<table>');
+	$('#barratable').attr('data-code', '</table>');
+	$('#thead').attr('data-code', '<thead>');
+	$('#barrathead').attr('data-code', '</thead>');
+	$('#tbody').attr('data-code', '<tbody>');
+	$('#barratbody').attr('data-code', '</tbody>');
+	$('#tr').attr('data-code', '<tr>');
+	$('#barratr').attr('data-code', '</tr>');
+	$('#th').attr('data-code', '<th>Cabeçalho</th>');
+	$('#td').attr('data-code', '<td>Coluna</td>');
+
+
+	
+
+
+
+	
+
+});
